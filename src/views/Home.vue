5103f11cb7a1bf20fdd145272f1dc9aab8035125
supersuperYang <l>
2018-07-26 15:56:58 +0800

    侧边栏

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,99 @@
 <template>
     <div class="home">
-      天不生我李淳罡,剑道万古长如夜!
+      <el-container>
+          <!-- 侧边栏 -->
+            <el-aside width="200px">
+                <div class="logo"></div>
+                <el-menu
+                default-active="2"
+                class="el-menu-admin"
+                @open="handleOpen"
+                @close="handleClose"
+                background-color="#545c64"
+                text-color="#fff"
+                active-text-color="#ffd04b">
+                    <el-submenu index="1">
+                        <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>用户管理</span>
+                        </template>                   
+                        <el-menu-item index="2">
+                            <i class="el-icon-menu"></i>
+                            <span slot="title">用户列表</span>
+                        </el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+            <el-container>
+                <el-header>Header</el-header>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
            
 <script>
-import {getUserList} from '@/api'
+import { getUserList } from "@/api";
 
-export default{
-    mounted() {
-        let params = {params:{query:'', pagenum:1, pagesize:1}}
-        getUserList(params).then(res =>{
-            console.log(res)
-        })
+export default {
+    methods: {
+    handleOpen(key, keyPath) {
+        console.log(key, keyPath)
+        },
+    handleClose(key, keyPath) {
+        console.log(key, keyPath)
+        }
     },
-}
+    mounted() {
+        let params = { params: { query: "", pagenum: 1, pagesize: 1 } };
+        getUserList(params).then(res => {
+        console.log(res);
+        });
+    }
+};
 </script>
-<style>
-            
+
+<style lang="scss" scoped>
+.home {
+  height: 100%;
+  .el-menu-admin:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .el-container {
+    height: 100%;
+  }
+  .el-aside {
+    background-color: #545c64;
+  }
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #545c64;
+  }
+  .logo {
+    height:60px;
+    background: url(../assets/logo.png);
+    background-size: cover;
+    background-color: #989898;
+  }
+  .toggle-btn {
+    font-size: 36px;
+    color: #989898;
+    cursor: pointer;
+    line-height: 60px;
+  }
+  .system-title {
+    font-size: 28px;
+    color: white;
+  }
+  .welcome, {
+    color: white;
+  }
+  
+}
+.el-menu{
+      border-right: 1px solid #545c64
+  }
 </style>
