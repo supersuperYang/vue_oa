07a106cc4b56bc07b45886eef0aaefab85b78ff8
supersuperYang <l>
2018-07-26 11:51:07 +0800

    LoginEnd

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-import '../styles/index.scss'
+import './styles/index.scss'
 
 
 Vue.use(ElementUI)
